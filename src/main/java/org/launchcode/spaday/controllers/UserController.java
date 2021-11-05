d8041f8ca200d62a8f9b535f6729c010e9d28e71
Nyssa <nyssagd@gmail.com>
2021-11-04 21:38:21 -0400

    "did the studiioish!"

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,6 +3,7 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,9 +19,11 @@ public class UserController {
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, String verify) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
 
-        if (user.getPassword().equals(verify)) {
+        if (errors.hasErrors()){
+            return "user/index";
+        }else if (user.getPassword().equals(verify)) {
            return "user/index";
         }
         else {
